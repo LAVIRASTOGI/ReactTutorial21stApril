4dc487f52250bc81f0c1bdbc1d0850f50cfcf20f
LaviRastogi <1993lavirastogi@gmail.com>
2025-04-22 11:34:53 +0530

    first commit

---
+++ lecture1.js
@@ -0,0 +1,21 @@
+// 1st -- import Export
+
+import apikey from "./util.js";
+
+// import { apikey } from "./util.js";
+//import { apikey, apikey1 } from "./util.js";
+//or
+
+//import * as api from "./util.js";
+// import apikey from "./util.js";
+
+//import { apikey, apikey1 as abc } from "./util.js";
+
+//console.log("apiKey", apikey);
+//console.log("apiKey", apikey,);
+//console.log("apiKey", api.apikey, api.apikey, api.apikey2);
+//console.log("apiKey", apikey, apikey, abc);
+
+// -------------------------------------------------------
+
+// console.log("apikey", apikey);
---
+++ util.js
@@ -0,0 +1,5 @@
+export const apikey = "123hfdjkse45nkllw";
+export const apikey1 = "123hfdjkse45nkllwdd";
+const apikey2 = "123hfdjkse45nkllw";
+
+export default "123hfdjkse45nkllw";
