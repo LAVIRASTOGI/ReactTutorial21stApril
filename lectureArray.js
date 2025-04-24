61aea7e6dd37f892410e60aef6826e9b4b341211
LaviRastogi <1993lavirastogi@gmail.com>
2025-04-24 15:54:03 +0530

    creating m ethods to teach

---
+++ lectureArray.js
@@ -0,0 +1,35 @@
+// // Creating Arrays
+
+// // Array literal (most common)
+// const fruits = ['apple', 'banana', 'orange'];
+
+// // Array constructor
+// const numbers = new Array(1, 2, 3, 4, 5);
+
+// // Creating an array with predefined length
+// const emptyArray = new Array(5); // Creates array with 5 empty slots
+
+// // Array.from() - creates array from array-like or iterable object
+// const arrayFromString = Array.from('hello'); // ['h', 'e', 'l', 'l', 'o']
+// const arrayFromSet = Array.from(new Set([1, 2, 2, 3])); // [1, 2, 3]
+
+// Basic Array Methods
+
+//push
+//pop
+//shift
+//unshift
+//splice
+//slice
+//includes
+
+// Array  Methods
+
+//map
+//filter
+//reduce
+//every
+//some
+//forEach
+//sort
+//.length
---
+++ lectureObj.js
@@ -0,0 +1,31 @@
+//Creating Objects
+
+//Accessing Object Properties
+
+// Modifying Objects
+
+// Adding new properties
+
+// Modifying existing properties
+
+// Deleting properties
+
+//Computed Property Names (ES6) - dynamic
+
+//Methods in Objects
+//Objects can contain functions as properties, which are called methods:
+
+//Object.freeze() and Object.seal()
+
+// Object.freeze() - prevents adding, removing, or changing properties
+// Object.seal() - prevents adding or removing properties, but allows changing existing ones
+
+//Property Shorthand (ES6)
+// Instead of { name: name, age: age, job: job }
+// const user = { name, age, job };
+
+//Object Destructuring (ES6)
+// Basic destructuring
+// Nested destructuring
+// Default values
+// Destructuring with new variable names
