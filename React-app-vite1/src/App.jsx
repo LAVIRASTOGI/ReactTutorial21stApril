60cdec7c56f47973ef03e436f849672ce809fb2c
LaviRastogi <1993lavirastogi@gmail.com>
2025-05-21 18:26:04 +0530

    raect

---
+++ React-app-vite1/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1> count is {count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
+
+// create fiber node
+
+// create fiber root node
+
+// code
+// rootFiber = {
+//   tag: HostRoot,
+//   stateNode: document.getElementById("root"),
+//   child: appFiber
+// }
+// code
+
+// appFiber = {
+//   tag: FunctionComponent,
+//   type: App,
+//   child: divFiber,
+//   return: rootFiber
+// }
+// code
+
+// divFiber = {
+//   tag: HostComponent,
+//   type: 'div',
+//   child: h1Fiber,
+//   return: appFiber,
+//   effectTag: 'Placement' // newly created node
+// };
+
+// h1Fiber = {
+//   tag: HostComponent,
+//   type: 'h1',
+//   sibling: buttonFiber,
+//   return: divFiber,
+//   effectTag: 'Placement'
+// };
+
+// buttonFiber = {
+//   tag: HostComponent,
+//   type: 'button',
+//   sibling: null,
+//   return: divFiber,
+//   effectTag: 'Placement'
+// };
